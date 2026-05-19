2d47c121b9fa9ba0109eba054ecc08772051a160
exal <exal59@yahoo.com>
2026-05-19 21:34:10 +0800

    Added Transcendental test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,24 @@
-module Main (main) where 
+module Main (main) where
 
-import           Test.Tasty
-import qualified SymHask.DifferentiaionSpec as Differentiation
-import qualified SymHask.SimplificationSpec as Simplification
-import qualified SymHask.IntegrationSpec as Integration
 import qualified SymHask.BasicSpec as Basic
+import qualified SymHask.DifferentiaionSpec as Differentiation
+import qualified SymHask.IntegrationSpec as Integration
 import qualified SymHask.PolynomialSpec as Polynomial
+import qualified SymHask.SimplificationSpec as Simplification
+import qualified SymHask.TranscendentalSpec as Transcendental
+import Test.Tasty
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "SymHask Tests"
-  [
-  Differentiation.tests,
-  Simplification.tests
- , Integration.tests
- , Basic.tests
- , Polynomial.tests
-  ]
+tests =
+  testGroup
+    "SymHask Tests"
+    [ Differentiation.tests,
+      Simplification.tests,
+      Integration.tests,
+      Basic.tests,
+      Polynomial.tests,
+      Transcendental.tests
+    ]
