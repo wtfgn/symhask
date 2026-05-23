1ebd21dd17fc5329747496ff0f3a8d229a413764
exal <exal59@yahoo.com>
2026-05-23 20:49:40 +0800

    Move trailSubTests to IntegrationSpec

--- test/SymHask/IntegrationSpec.hs
+++ test/SymHask/IntegrationSpec.hs
@@ -9,11 +9,13 @@ import           SymHask.Symbolic                      (UnsimplifiedExpr,
                                                         mkSymbol)
 import           SymHask.Symbolic.Calculus.Integration (integrate,
                                                         integrateLinear,
-                                                        integrateTable)
+                                                        integrateTable,
+                                                        trialSubstitutions)
 import           Test.Tasty                            (TestTree, testGroup)
 import           Test.Tasty.HUnit                      (assertFailure, testCase,
                                                         (@?=))
 import           TestUtils                             (simplifyOrFail)
+import qualified Data.HashSet                           as HS
 
 tests :: TestTree
 tests =
@@ -22,6 +24,7 @@ tests =
     [ integrationTableTests
     , linearPropertiesTests
     , integrateTests
+    , trialSubTests
     ]
 
 -- ============================================================================
@@ -117,3 +120,43 @@ integrateTests =
                  in runIntegrate integrand @?= Just (simplifyOrFail expected)
             ]
     ]
+
+-- ============================================================================
+-- Trial Substitutions
+-- ============================================================================
+
+trialSubTests :: TestTree
+trialSubTests =
+    testGroup
+        "trialSubstitutions"
+        [ let x = mkSymbol "x" :: UnsimplifiedExpr
+              run e = trialSubstitutions (simplifyOrFail e)
+           in testGroup
+                "candidates"
+                [ testCase "sin(x) yields sin(x) and x" $
+                    let s = run (sin x)
+                     in (HS.member (simplifyOrFail (sin x)) s @?= True) >> (HS.member (simplifyOrFail x) s @?= True)
+                , testCase "x^2 yields base x and exponent 2" $
+                    let s = run (x ** (2 :: UnsimplifiedExpr))
+                     in (HS.member (simplifyOrFail x) s @?= True) >> (HS.member (simplifyOrFail (2 :: UnsimplifiedExpr)) s @?= True)
+                , testCase "(sin x)^2 yields sin(x) and x and 2" $
+                    let s = run (sin x ** (2 :: UnsimplifiedExpr))
+                     in (HS.member (simplifyOrFail (sin x)) s @?= True) >> (HS.member (simplifyOrFail x) s @?= True) >> (HS.member (simplifyOrFail (2 :: UnsimplifiedExpr)) s @?= True)
+                , testCase "complex integrand: (x+1)*ln(cos((x+1)^2))*sin((x+1)^2) / (cos((x+1)^2))" $
+                    let xPlus1 = x + 1
+                        innerPower = xPlus1 ** (2 :: UnsimplifiedExpr)
+                        cosInner = cos innerPower
+                        logCos = log cosInner
+                        sinCos = sin cosInner
+                        expr = xPlus1 * logCos * sinCos / cosInner
+                        s = run expr
+                     in do
+                            HS.member (simplifyOrFail logCos) s @?= True
+                            HS.member (simplifyOrFail cosInner) s @?= True
+                            HS.member (simplifyOrFail innerPower) s @?= True
+                            HS.member (simplifyOrFail xPlus1) s @?= True
+                            HS.member (simplifyOrFail sinCos) s @?= True
+                            HS.member (simplifyOrFail (-1 :: UnsimplifiedExpr)) s @?= True
+                            HS.member (simplifyOrFail (2 :: UnsimplifiedExpr)) s @?= True
+                ]
+        ]
